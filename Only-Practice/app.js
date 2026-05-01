698a8144d04aac543e55565488d1a2b462b52d58
Gautam Thakur <mnrhag5@gmail.com>
2026-05-01 12:38:36 +0530

    add

--- Only-Practice/app.js
+++ Only-Practice/app.js
@@ -1,2 +1,12 @@
 //mere purani practice wala folder delete ho gya hai usme koi 1500+ lines thi.
 //to ab shuru se shuru krte hai-----------------
+
+//---------set time out method----------
+
+console.log('learning coding');
+
+setTimeout(()=>{
+    console.log('learning Asynchronous JavaScript');
+
+},2000)
+console.log('hello world');

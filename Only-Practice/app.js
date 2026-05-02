82a84028838af184f806636237653ba76fc2da68
Gautam Thakur <mnrhag5@gmail.com>
2026-05-02 11:54:22 +0530

    add

--- Only-Practice/app.js
+++ Only-Practice/app.js
@@ -3,10 +3,23 @@
 
 //---------set time out method----------
 
-console.log('learning coding');
+// console.log('learning coding');
 
-setTimeout(()=>{
-    console.log('learning Asynchronous JavaScript');
+// setTimeout(()=>{
+//     console.log('learning Asynchronous JavaScript');
 
-},2000)
-console.log('hello world');
+// },2000)
+// console.log('hello world');
+
+//---------callback--------------------
+
+
+
+let arr=[1,2,3,4,5,6];
+
+arr.push(7,8,9);
+arr.pop('9');
+arr.unshift(-1,0);
+arr.shift();
+
+console.log(arr);

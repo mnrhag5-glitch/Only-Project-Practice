31c2e49a3e6235826f7c993da441d19ce9af7add
Gautam Thakur <mnrhag5@gmail.com>
2026-05-02 12:26:20 +0530

    add

--- Only-Practice/app.js
+++ Only-Practice/app.js
@@ -15,11 +15,49 @@
 
 
 
+// let arr=[1,2,3,4,5,6];
+
+// arr.push(7,8,9);
+// arr.pop('9');
+// arr.unshift(-1,0);
+// arr.shift();
+
+// console.log(arr);
+
+
+
+// function small(){
+// console.log('small function');
+// }
+
+// function big(fn){
+//     console.log('big function'); 
+//     fn();
+// }
+
+// big(small);
+
+
+//------------------------------
+
+// function outer(){
+//     console.log('outer function');
+
+//     function inner(){
+//         console.log('inner function');
+//     }
+//     return inner;
+// }
+// outer();
+
+
+
+// let arr=['apple','banana','grapes','orange'];
+// arr.forEach((item,index)=>{
+//     console.log(item,index);
+// })
+
 let arr=[1,2,3,4,5,6];
-
-arr.push(7,8,9);
-arr.pop('9');
-arr.unshift(-1,0);
-arr.shift();
-
-console.log(arr);
+arr.forEach((i,index)=>{
+    console.log(i ,'=>',index);
+})
